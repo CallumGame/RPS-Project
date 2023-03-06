5d8405b36bf70129807303333d3e856a95d86a4f
CallumGame <callumutd@live.co.uk>
2023-03-06 12:56:12 +0300

    added functions to check winner and responses to who wins

--- RPS.js
+++ RPS.js
@@ -9,3 +9,33 @@ function getComputerChoice(){
     return(random)
 }
 console.log(getComputerChoice())
+
+function checkWinner(playerSelection,computerSelection){
+if(playerSelection == computerSelection){
+return "Draw";
+}
+else if( (playerSelection.tolowercase == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")){
+    return "player";
+}
+else {
+    return "computer"
+ }
+}
+
+
+
+
+function playRound(playerSelection,computerSelection){
+  const result = checkwinner(playerSelection,computerSelection);
+  if(result == "draw"){
+  return "Draw play again";}
+  else if(result == "player") {
+    return "You win"}
+else(result == "computer")
+    return "DSJJSHDJS Computer wins"
+
+
+
+    }
